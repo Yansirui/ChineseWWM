03ff2de9b12f1b9bd66342325ae473064f351199
Yansirui <118953128+Yansirui@users.noreply.github.com>
2023-10-30 13:42:18 +0800

    Update Random_and_Whole_data_gen.py

--- Random_and_Whole_data_gen.py
+++ Random_and_Whole_data_gen.py
@@ -2,12 +2,13 @@ import random
 import bisect
 import json
 
-
+#读取json文件
 def read_json(json_path):
     with open(json_path, 'r', encoding='utf-8') as f:
         data = json.load(f)
         return data
 
+#找到sub在string中的所有起始位置
 def find_all(string, sub):
     start = 0
     pos = []
@@ -18,18 +19,25 @@ def find_all(string, sub):
         pos.append(start)
         start += len(sub)
 
+#读取txt文件，去掉多余符号
 def read_txt(file_path):
     with open(file_path, 'r', encoding='utf-8') as f:
         result = f.readlines()
     return [line.strip() for line in result]
 
+#根据所给terms列表返回被掩码的sentence(eg:terms=['北京'] --> 我想要去北京去看北京天安门 --> 我想要去♥♥去看♥♥天安门)，同时如果掩码率不足0.15，则采用随机掩码策略
 def whole_mask(sentence, terms):
+    #term按照长度排序，比如北京天安门，如果词表中有北京天安门，应该优先考虑北京天安门而不是北京
     terms=sorted(terms,key=lambda i:len(i),reverse=True)
+    #分别为词掩码符号和token掩码符号
     masked = '♥'
     masked1 = '☕'
     sentence = sentence.replace('\n', '')
+    #记录起始位置，用于判断是否找到重合的词汇：如北京天安门，我已经找到北京天安门了，但是我还有'北京'这个词汇，利用start记录判断当前北京词汇是否用于其他词汇
+    #为什么不直接找到该词汇就用masked替换呢，因为要控制掩码率！
     start = []
     end = []
+    #被掩码的terms列表以及token列表
     sentence_masked_terms = []
     sentence_masked_words=[]
     i=0
@@ -37,10 +45,13 @@ def whole_mask(sentence, terms):
         if term == '':
             break
         length = len(term)
+        #找到词汇起始位置
         positions = find_all(sentence, term)
         for pos in positions:
+            #是否可以作为掩码的候选词汇，True代表可以
             kequ=True
             for s,e in zip(start,end):
+                #不可以作为候选词汇的条件，与已选词汇发生重叠
                 if(s <  pos and pos <  e) or (s < pos + length and pos + length < e) or (pos < s and pos+length > e) or (pos==s):
                     kequ=False
             if kequ:
@@ -48,9 +59,12 @@ def whole_mask(sentence, terms):
                 end.append(pos + length)
             else:
                 continue
+    #掩码率：0.15，可自己修改
     mask = len(sentence) * 0.15
     start_index = []
     term_length = []
+    
+    #如果该句没有词表内词汇，则选择用随机掩码
     if len(start) == 0:
         random_int=[]
         while mask > 0 :
@@ -66,26 +80,33 @@ def whole_mask(sentence, terms):
             sentence_masked_words.append(sentence[index])
         return ''.join(sentence_list),term_length,sentence_masked_terms,sentence_masked_words
 
-
+    #有词表中的词汇
     while mask > 0:
+        #随机挑选某个词
         index = random.randint(0, len(start) - 1)
+        #保存当前正在被掩码的词汇
         word = sentence[start[index]:end[index]]
         mylist = list(sentence)
         j = start[index]
+        #将词汇掩码为masked(♥)
         while j < end[index]:
             mylist[j] = masked
             j += 1
         sentence = ''.join(mylist)
+        #start_index加入start[index]，为什么用这个不用append，因为要保证start小的在前面
         bisect.insort(start_index, start[index])
         insort_index = start_index.index(start[index])
         term_length.insert(insort_index, end[index] - start[index])
         sentence_masked_terms.insert(insort_index, word)
+        #更新mask剩余数量
         mask -= (end[index] - start[index])
         start.pop(index)
         end.pop(index)
         if len(start) == 0:
             break
     random_int=[]
+    #如果mask的掩码率没有达到标准，继续使用随即掩码
+    #为什么不一边随机挑选index一边掩码，因为需要顺序，所以需要先挑选，再排序，再掩码
     while mask > 0:
         word_num = random.randint(0, len(sentence) - 1)
         while word_num in random_int or sentence[word_num] == '♥':
@@ -103,13 +124,12 @@ def whole_mask(sentence, terms):
         return sentence, term_length, sentence_masked_terms,sentence_masked_words
 
 import re
+#根据设置的最长句子长度来进行分句
 def split_sentences(sentence):
     MAX_LENGTH = 500
     sentences = []
+    #分句的依据，多个中文结句符号
     sub_sentences=re.split(r'([。！!？?])',sentence)
-    #sub_sentences.append("")
-    #sub_sentences = ["".join(i) for i in zip(sentences[0::2],sentences[1::2])]
-    #sub_sentences = sentence.split('，')
     cur_sentence = ''
     for sub in sub_sentences:
         if len(cur_sentence) + len(sub) > MAX_LENGTH:
@@ -124,7 +144,7 @@ def split_sentences(sentence):
         sentences.append(cur_sentence)
     return sentences
 
-
+#添加空白，在被掩码符号的前后加上空格，因为在做tokenizer的时候，♥[UNK]-->[UNK]   如果加上空格后 ♥ [UNK]-->♥[UNK]，有时候会将掩码符号和后面的不知名符号连接起来组成一个不知名符号
 def add_blank(content):
     sentence=''
     i=0
@@ -144,11 +164,13 @@ def add_blank(content):
             sentence=sentence+sen
     return sentence
 nums=1
+#词表文件，一行一个词
 term_path=r'/home/sirui/WMM/Medicine/baike_data/WWM_terms.txt'
 with open(term_path,'r',encoding='utf-8') as f:
     terms=f.readlines()
 for i in range(len(terms)):
     terms[i]=terms[i].replace('\n','')
+#去重
 terms=list(set(terms))
 #terms=[]
 print(len(terms))
@@ -165,6 +187,7 @@ with open(r'/home/sirui/WMM/Medicine/baike_data/baike_corpus.txt', 'r', encoding
         sentences = (line.strip() for line in f)
         for sentence in sentences:
             sentence = sentence.replace('♥', '').replace('☕','')
+            #去除所有空格，因为不去除有时候会随机选择到空格进行掩码
             sentence = re.sub(r'\s','',sentence)
             if len(sentence) > 500:
                 sub_sentences = split_sentences(sentence)
